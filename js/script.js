8c69e6336d505f4a98700efb5b1cb29273400ec3
MrGians <gianlumura97@gmail.com>
2022-06-30 13:36:15 +0200

    Boilerplate

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// TRACCIA
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Al click sulla cella, essa si colora di azzurro e ne viene stampato in console il numero corrispondente.
+
+// # MILESTONE 1
+// Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
+
+// # MILESTONE 2
+// Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS.
+// Al click del bottone "Play" vengono generate 100 celle in 10 righe da 10 celle ciascuna.
+
+// # MILESTONE 3
+// In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
+
+// # MILESTONE 4
+// Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro.
+
+// # BONUS
+// Aggiungere una select accanto al bottone "Play", che fornisca una scelta tra tre diversi livelli di difficoltà:
+// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+
+
